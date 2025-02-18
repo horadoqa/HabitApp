d6bc57764a8539c5108574d18e8498cb29805ec0
Ricardo Fahham <rfahham@hotmail.com>
2025-02-18 18:59:19 -0300

    redirecionando para login depois do cadastro

--- fe/habitatapp/src/pages/Usuario/Cadastrar.js
+++ fe/habitatapp/src/pages/Usuario/Cadastrar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import { useNavigate } from 'react-router-dom';  // Importar o hook useNavigate
 
 const Cadastro = () => {
   const [nome, setNome] = useState('');
@@ -8,6 +8,8 @@ const Cadastro = () => {
   const [senha, setSenha] = useState('');
   const [success, setSuccess] = useState('');
   const [error, setError] = useState('');
+  
+  const navigate = useNavigate();  // Iniciar o useNavigate
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,6 +21,11 @@ const Cadastro = () => {
       if (response.status === 201) {
         setSuccess(response.data.message);
         setError('');
+        
+        // Após o sucesso, redireciona para a página de login
+        setTimeout(() => {
+          navigate('/');  // Redireciona para a página de login
+        }, 2000);  // Espera 2 segundos antes de redirecionar
       } else {
         setError('Erro ao cadastrar. Tente novamente.');
         setSuccess('');
@@ -32,42 +39,42 @@ const Cadastro = () => {
 
   return (
     <div className="container">
-    <div>
-      <h2>Cadastro de Usuário</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Nome:</label>
-          <input
-            type="text"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>E-mail:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Senha:</label>
-          <input
-            type="password"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Cadastrar</button>
-      </form>
+      <div>
+        <h2>Cadastro de Usuário</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Nome:</label>
+            <input
+              type="text"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>E-mail:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Senha:</label>
+            <input
+              type="password"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Cadastrar</button>
+        </form>
 
-      {success && <div style={{ color: 'green' }}>{success}</div>}
-      {error && <div style={{ color: 'red' }}>{error}</div>}
-    </div>
+        {success && <div style={{ color: 'green' }}>{success}</div>}
+        {error && <div style={{ color: 'red' }}>{error}</div>}
+      </div>
     </div>
   );
 };
