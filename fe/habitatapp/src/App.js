d97d70fb76420599d02783e8be1901839362f376
Ricardo Fahham <rfahham@hotmail.com>
2025-02-18 09:30:20 -0300

    Organizando app.js com as rotas

--- fe/habitatapp/src/App.js
+++ fe/habitatapp/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HealthCheck from './pages/HealthCheck';
 import Login from './pages/Login';
 import Usuario from './pages/Usuario/Cadastrar';
-import Apartamento from './pages/Apartamento';
+import Apartamento from './pages/Apartamento/Cadastrar';
 // import Home from './pages/Home';
 
 const App = () => {
@@ -12,14 +12,19 @@ const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/healthcheck" element={<HealthCheck />} />
+        
+        {/* <Route path="/home" element={<Home />} /> */}
+        
         {/* Rota para Login */}
         <Route path="/" element={<Login setToken={setToken} />} />
-        
-        {/* Rota para Cadastro */}
-        <Route path="/healthcheck" element={<HealthCheck />} />
+                
+        {/* Rota para Usuário*/}
         <Route path="/usuario" element={<Usuario />} />
-        <Route path="/apartamento-cadastrar" element={<Apartamento />} />
-        {/* <Route path="/home" element={<Home />} /> */}
+        
+        {/* Rota para Apartamento*/}
+        <Route path="/apartamento" element={<Apartamento />} />
+
       </Routes>
     </Router>
   );
